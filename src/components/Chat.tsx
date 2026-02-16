89e0db4264af93b425978c8b2e0752026cf30a06
Ted Nunes”git config --global user.email “tedmnunes@gmail.com <tedmnunes@gmail.com>
2026-02-15 21:15:34 -0500

    Text back to gray, keep coral icon

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -73,7 +73,7 @@ function ResultsPanel({ results }: { results: ToolResult[] }) {
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1.5} d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
             </svg>
           </div>
-          <p className="text-sm text-[#E07A5F]">Results will show here</p>
+          <p className="text-sm text-gray-400">Results will show here</p>
         </div>
       </div>
     )
